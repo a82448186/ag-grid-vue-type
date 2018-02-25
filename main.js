032817f16a0e2ccec2ca27816ea173d77da7957f
a82448186 <464969626@qq.com>
2018-02-25 09:57:28 +0800

    修改main.js 中export方法

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 
 var _agGridVue = require("./dist/agGridVue");
 
-export default {AgGridVue:_agGridVue}
+Object.defineProperty(exports, "AgGridVue", {
+    enumerable: true,
+    get: function get() {
+        return _interopRequireDefault(_agGridVue).default;
+    }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
